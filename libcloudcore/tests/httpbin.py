876b4fa6b7e1c2e585e29fced961030a2f006d66
John Carr <john.carr@unrouted.co.uk>
2015-07-22 08:08:26 +0100

    Absolute imports for py2.x

--- libcloudcore/tests/httpbin.py
+++ libcloudcore/tests/httpbin.py
@@ -13,6 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from __future__ import absolute_import
+
 import threading
 import unittest
 from wsgiref.simple_server import make_server
